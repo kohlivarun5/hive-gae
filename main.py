c69e5a1e7ac03ff1b7879fd17a39675b765c0dcf
Varun Kohli <kohli_varun5@yahoo.com>
2014-06-25 22:22:44 -0400

    .

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-./web-app/main.py
+# Add the library location to the path
+import sys
+sys.path.insert(0, '_libs')
+sys.path.insert(0, '_modules')
+
+import webapp2
+
+# Import all All handler modules
+import Home
+import Oauth
+import Subscriptions
+import Cron
+
+ROUTES = (
+    Home.ROUTES
+  + Oauth.ROUTES
+  + Subscriptions.ROUTES
+  + Cron.ROUTES
+  )
+
+app = webapp2.WSGIApplication(ROUTES)
